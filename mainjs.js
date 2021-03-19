e1664d4f42925a366912601b763da06947262946
Sa4eMmNeNiK <71568108+Sa4eMmNeNiK@users.noreply.github.com>
2021-03-19 20:52:33 +0300

    Add files via upload

--- mainjs.js
+++ mainjs.js
@@ -8,7 +8,7 @@ var tegOffice = document.querySelector(".tegInp");
 confirmButton.onclick = function(){
     if (postOffice.includes(password.value) && passTeg.includes(tegOffice.value)){
         alert('Привет Студент!');
-        window.location.replace('file:///D:/web/index2.html')
+        window.location.replace('index2.html')
 } else if (password.value == '' || tegOffice.value == null || tegOffice.value == '' || password.value == null){
     alert('Error 404');
 } else {
