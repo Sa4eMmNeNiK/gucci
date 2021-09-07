f0db372dbacf40dc65e2604f4dd75b0d2af1e5ef
Sa4eMmNeNiK <71568108+Sa4eMmNeNiK@users.noreply.github.com>
2021-09-07 15:30:16 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-var doki = document.querySelector('#doki');
-var box1 = document.querySelectorAll('.box1');
+// const box = document.querySelector('.contentTabs');
+// const tabs = document.querySelectorAll('.tabs');
+// const content = document.querySelectorAll('.img');
 
-for(let i = 0; i < box1.length; i++) {
-    box1[i].addEventListener('click', function(e){
-        if(this.style.transform == 'rotateY(0deg)') this.style.transform = 'rotateY(180deg)';
-        else this.style.transform = 'rotateY(0deg)';
-    });
-}
+// box.onclick = function(e){
+//     console.log(e.target.dataset.tab);
+//     tabs.forEach(item => item.classList.remove('active') );
+//     e.target.classList.add('active');
+//     content.forEach(item => item.classList.remove('active'));
+//     content[e.target.dataset.tab].classList.add('active');
+// }
 
+
+let bagger = document.querySelector('.bagger');
+bagger.onclick = function(){
+    for(let a = 1; a>0; ++a){
+        console.log(a + this.bagger);
+    }
+};
 
